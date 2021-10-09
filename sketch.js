4db5afa36f74f5ad7c6f84a415107e5cbf11f65b
BrownHatJo <92066157+BrownHatJo@users.noreply.github.com>
2021-10-08 22:04:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var marks = [1, 4, 5, 6 , 9, 2 , 3, 5, 6, 9]
+console.log(marks[4])
+
+var sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4]+marks[5]+marks[6]+marks[7]+marks[8]+marks[9]
+console.log(marks.length)
+
+var average = sum/marks.length
+console.log(average)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
